a0a8b18b479b25abde4e45346da11e9108be3edf
dougtebay <dougtebay@gmail.com>
2015-12-17 19:56:56 -0500

    All tests passing

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,16 @@
 # Enter your procedural solution here!
+def largest_prime_factor(num)
+  counter = 1
+  while num > counter
+    counter += 1
+    num = num / counter if num % counter == 0 && is_prime?(counter)
+  end
+  counter
+end
+
+def is_prime?(num)
+  (Math.sqrt(num).to_i).downto(2) { |i| return false if num % i == 0 }
+  return true
+end
+
+largest_prime_factor(600851475143)
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,20 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+  def initialize(num)
+    @counter = 1
+    while num > @counter
+      @counter += 1
+      num = num / @counter if num % @counter == 0 && is_prime?(@counter)
+    end
+    @counter
+  end
+
+  def is_prime?(num)
+    (Math.sqrt(num).to_i).downto(2) { |i| return false if num % i == 0 }
+    return true
+  end
+
+  def number
+    @counter
+  end
+end
